50951c35862c88c6d2bdc4a3541d78ff36681d96
baaraban <perepichka@ucu.edu.ua>
2020-05-14 12:51:35 +0300

    Add Dal

---
+++ KanbanBoard/KanbanBoard/Repositories/Abstraction/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Repositories.Abstraction
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        IQueryable<TEntity> AsQueryable();
+
+        TEntity Get(object id);
+        TEntity Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ KanbanBoard/KanbanBoard/Repositories/Implementation/Repository.cs
@@ -0,0 +1,46 @@
+﻿using KanbanBoard.Repositories.Abstraction;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KanbanBoard.Repositories.Implementation
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext context;
+        private readonly DbSet<TEntity> table;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            this.table = this.context.Set<TEntity>();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            this.table.Add(entity);
+            this.context.SaveChanges();
+            return entity;
+        }
+
+        public IQueryable<TEntity> AsQueryable() => this.table.AsQueryable<TEntity>();
+
+        public void Delete(TEntity entity)
+        {
+            this.table.Remove(entity);
+            this.context.SaveChanges();
+        }
+
+        public TEntity Get(object id) => this.table.Find(id);
+
+        public IEnumerable<TEntity> GetAll() => this.table.ToList();
+
+        public TEntity Update(TEntity entity)
+        {
+            this.table.Attach(entity);
+            this.context.Entry(entity).State = EntityState.Modified;
+            this.context.SaveChanges();
+            return entity;
+        }
+    }
+}
